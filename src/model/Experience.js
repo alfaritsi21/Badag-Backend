92ef9bd25ea74f4965493d08676e606e7af00e7e
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-20 23:01:02 +0700

    update image

--- src/model/Experience.js
+++ src/model/Experience.js
@@ -1,55 +1,66 @@
-const connection = require("../config/mysql")
+const connection = require("../config/mysql");
 
 module.exports = {
+  getExperienceByUserId: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT id,id_user,company,position, DATE_FORMAT(date, "%Y-%m-%d") AS date_work, DATE_FORMAT(date_resign, "%Y-%m-%d") AS resign, description FROM `experiences` WHERE id_user = ?',
+        id,
+        (error, result) => {
+          !error ? resolve(result) : reject(new Error(error));
+        }
+      );
+    });
+  },
 
-    getExperienceByUserId: (id) => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT id,id_user,company,position, DATE_FORMAT(date, "%Y-%m-%d") AS date_work, DATE_FORMAT(date_resign, "%Y-%m-%d") AS resign, description FROM `experiences` WHERE id_user=1', id, (error, result) => {
-                !error ? resolve(result) : reject(new Error(error))
-            })
-        })
-    },
+  postExperience: (data) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "INSERT INTO experiences SET ?",
+        data,
+        (error, result) => {
+          if (!error) {
+            const newResult = {
+              id: result.insertId,
+              ...data,
+            };
+            resolve(newResult);
+          } else {
+            reject(new Error(error));
+          }
+        }
+      );
+    });
+  },
 
-    postExperience: (data) => {
-        return new Promise((resolve, reject) => {
-            connection.query("INSERT INTO experiences SET ?", data, (error, result) => {
-                if (!error) {
-                    const newResult = {
-                        id: result.insertId,
-                        ...data,
-                    };
-                    resolve(newResult);
-                } else {
-                    reject(new Error(error));
-                }
-            })
-        })
-    },
+  getExperienceById: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "SELECT * FROM experiences WHERE id = ?",
+        id,
+        (error, result) => {
+          !error ? resolve(result) : reject(new Error(error));
+        }
+      );
+    });
+  },
 
-    getExperienceById: (id) => {
-        return new Promise((resolve, reject) => {
-            connection.query("SELECT * FROM experiences WHERE id = ?", id, (error, result) => {
-                !error ? resolve(result) : reject(new Error(error))
-            })
-        })
-    },
-
-    deleteExperience: (id) => {
-        return new Promise((resolve, reject) => {
-            connection.query(
-                "DELETE FROM experiences WHERE id = ?",
-                id,
-                (error, result) => {
-                    if (!error) {
-                        const newResult = {
-                            id: id,
-                        };
-                        resolve(newResult);
-                    } else {
-                        reject(new Error(error));
-                    }
-                }
-            );
-        });
-    },
-}
+  deleteExperience: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "DELETE FROM experiences WHERE id = ?",
+        id,
+        (error, result) => {
+          if (!error) {
+            const newResult = {
+              id: id,
+            };
+            resolve(newResult);
+          } else {
+            reject(new Error(error));
+          }
+        }
+      );
+    });
+  },
+};
