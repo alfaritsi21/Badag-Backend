dbb0f9d8569962a741687cc44adc22d7af428f5d
careerdimasprayoga <career.dimasprayoga@gmail.com>
2020-09-21 00:27:20 +0700

    Modify Chat for frontend

--- src/model/chat/chat.js
+++ src/model/chat/chat.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   get_message: (id_roomchat) => {
     return new Promise((resolve, reject) => {
-      connection.query("SELECT company.company_id, company.company_name, company.company_image, message.message, message.created_at, message.sender, message.receive FROM message INNER JOIN roomchat on roomchat.id_roomchat = message.roomchat_id INNER JOIN company on company.company_id = roomchat.company_id WHERE roomchat.id_roomchat = ?", id_roomchat, (error, result) => {
+      connection.query("SELECT company.company_id, company.company_name, company.company_image, message.message, message.created_at, message.sender, message.receive, company.role_id AS roleid_company, users.role_id AS roleid_user FROM message INNER JOIN roomchat on roomchat.id_roomchat = message.roomchat_id INNER JOIN company on company.company_id = roomchat.company_id LEFT JOIN users ON users.user_id = roomchat.user_id WHERE roomchat.id_roomchat = ?", id_roomchat, (error, result) => {
         !error ? resolve(result) : reject(new Error(error));
       });
     });
