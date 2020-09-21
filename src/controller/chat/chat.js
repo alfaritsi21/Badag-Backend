0215cfc0c5e77a868e1d93ed05118a8fe3085b0a
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-21 20:45:41 +0700

    Update syntax

--- src/controller/chat/chat.js
+++ src/controller/chat/chat.js
@@ -1,62 +1,80 @@
-const helper = require('../../helper/index')
-const redis = require('redis')
-const client = redis.createClient()
-const { get_room_chat_company, get_room_chat_worker, get_message, post_message_model } = require('../../model/chat/chat')
-const { sendNotifMessage } = require('../../model/navbar/Navbar')
+const helper = require("../../helper/index");
+const redis = require("redis");
+const client = redis.createClient();
+const {
+  get_room_chat_company,
+  get_room_chat_worker,
+  get_message,
+  post_message_model,
+} = require("../../model/chat/chat");
+const { sendNotifMessage } = require("../../model/navbar/Navbar");
 
 module.exports = {
   get_chat_room_company: async (request, response) => {
-    let { company_id } = request.body
+    let { company_id } = request.body;
     try {
-      const result = await get_room_chat_company(company_id)
-      client.setex(`get_chatroom_company,company_id:${company_id}`, 3600, JSON.stringify(result))
-      return helper.response(response, 200, "Get Chat Success", result)
+      const result = await get_room_chat_company(company_id);
+      client.setex(
+        `get_chatroom_company,company_id:${company_id}`,
+        3600,
+        JSON.stringify(result)
+      );
+      return helper.response(response, 200, "Get Chat Success", result);
     } catch (error) {
-      return helper.response(response, 400, "Bad Request", error)
+      return helper.response(response, 400, "Bad Request", error);
     }
   },
   get_chat_room_worker: async (request, response) => {
-    let { user_id } = request.body
+    let { user_id } = request.body;
     try {
-      const result = await get_room_chat_worker(user_id)
-      client.setex(`get_chatroom_worker,worker_id:${user_id}`, 3600, JSON.stringify(result))
-      return helper.response(response, 200, "Get Chat Success", result)
+      const result = await get_room_chat_worker(user_id);
+      client.setex(
+        `get_chatroom_worker,worker_id:${user_id}`,
+        3600,
+        JSON.stringify(result)
+      );
+      return helper.response(response, 200, "Get Chat Success", result);
     } catch (error) {
-      return helper.response(response, 400, "Bad Request", error)
+      return helper.response(response, 400, "Bad Request", error);
     }
   },
   get_message: async (request, response) => {
-    let { roomchat_id } = request.body
+    let { roomchat_id } = request.body;
     try {
-      const result = await get_message(roomchat_id)
-      client.setex(`get_message,roomchat_id:${roomchat_id}`, 3600, JSON.stringify(result))
-      return helper.response(response, 200, "Get Message Success", result)
+      const result = await get_message(roomchat_id);
+      client.setex(
+        `get_message,roomchat_id:${roomchat_id}`,
+        3600,
+        JSON.stringify(result)
+      );
+      return helper.response(response, 200, "Get Message Success", result);
     } catch (error) {
-      return helper.response(response, 400, "Bad Request", error)
+      return helper.response(response, 400, "Bad Request", error);
     }
   },
   post_message: async (request, response) => {
-    let { roomchat_id, sender, receive, message, role_id } = request.body // company / worker login
+    const { send } = request.params;
+    let { roomchat_id, sender, receive, message, role_id } = request.body; // company / worker login
     try {
       const setData = {
         roomchat_id: roomchat_id,
         sender: sender,
         receive: receive,
         message: message,
-        created_at: new Date()
-      }
+        created_at: new Date(),
+      };
       const dataNotif = {
         title: 1,
         id_sender: sender,
-        id_worker: role_id === '1' ? receive : null,
-        id_company: role_id === '2' ? receive : null,
-        status: 0
-      }
-      await sendNotifMessage(dataNotif)
-      const result = await post_message_model(setData)
+        id_worker: role_id === "1" ? receive : null,
+        id_company: role_id === "2" ? receive : null,
+        status: 0,
+      };
+      await sendNotifMessage(dataNotif);
+      const result = await post_message_model(setData);
       return helper.response(response, 201, "Send Chat Success", result);
     } catch (error) {
       return helper.response(response, 400, "Bad Request", error);
     }
-  }
-}
+  },
+};
--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,11 +1,20 @@
-const router = require('express').Router()
-const { get_chat_room_company, get_chat_room_worker, get_message, post_message } = require('../controller/chat/chat')
+const router = require("express").Router();
+const {
+  get_chat_room_company,
+  get_chat_room_worker,
+  get_message,
+  post_message,
+} = require("../controller/chat/chat");
 const { authUser } = require("../middleware/Auth");
-const { get_chat_room_company_redis, get_chat_room_worker_redis, clearDataRedis } = require('../middleware/Redis')
+const {
+  get_chat_room_company_redis,
+  get_chat_room_worker_redis,
+  clearDataRedis,
+} = require("../middleware/Redis");
 
-router.get("/company", authUser, get_chat_room_company_redis, get_chat_room_company)
-router.get("/worker", authUser, get_chat_room_worker_redis, get_chat_room_worker)
-router.get("/", authUser, get_message)
-router.post("/", authUser, clearDataRedis, post_message)
+router.post("/company", authUser, clearDataRedis, get_chat_room_company);
+router.post("/worker", authUser, clearDataRedis, get_chat_room_worker);
+router.post("/", authUser, clearDataRedis, get_message);
+router.post("/send", authUser, clearDataRedis, post_message);
 
-module.exports = router
+module.exports = router;
